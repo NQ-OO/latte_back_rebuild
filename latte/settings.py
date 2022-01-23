0a958beffeb4ce7bbfc6924235ae3d694a842fae
NQ-OO <lol1219@snu.ac.kr>
2022-01-24 05:07:33 +0900

    [chore] Change semi-deploy mode - use mysql

--- latte/settings.py
+++ latte/settings.py
@@ -101,12 +101,24 @@ WSGI_APPLICATION = 'latte.wsgi.application'
 #         'PORT' : '3306'
 #         },
 #     }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'site1',
+        'USER': 'latteadmin',
+        'HOST' : '172.17.0.1',
+        'PASSWORD' : '123456789',
+        'PORT' : '3306'
+        },
     }
-}
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 
 
