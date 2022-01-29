98f98dd5218d3374c3920a4e9b0c9e2f9eb937cb
NQ-OO <lol1219@snu.ac.kr>
2022-01-29 13:56:19 +0900

    [chore] Add Like model to admin

--- latte/admin.py
+++ latte/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Quest, School, Category, Done
+from .models import Quest, School, Category, Done, Like
 
 admin.site.register(Quest)
 admin.site.register(School)
 admin.site.register(Category)
 admin.site.register(Done)
+admin.site.register(Like)
 
 
 
